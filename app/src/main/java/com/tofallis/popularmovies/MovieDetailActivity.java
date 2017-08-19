4b33a6e0360a95babc15a5286d23e0e427273f81
Aristos Tofallis <tofallis@gmail.com>
2017-08-19 11:19:16 +0100

    Empty project

---
+++ app/src/main/java/com/tofallis/popularmovies/MovieDetailActivity.java
@@ -0,0 +1,13 @@
+package com.tofallis.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
